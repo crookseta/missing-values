fbf7f9b8213484a580f73e66ea8b0ac362792ca6
crookseta <pacedeno.2002@gmail.com>
2026-03-29 21:18:08 -0500

    Fix incorrect comparison on Vector256 path for 512-bit integers.

--- src/MissingValues/UInt512.Implementations.cs
+++ src/MissingValues/UInt512.Implementations.cs
@@ -1565,8 +1565,8 @@ namespace MissingValues
 				Vector256<ulong> vUpper1 = Vector256.Create(left._p4, left._p5, left._p6, left._p7);
 				Vector256<ulong> vLower1 = Vector256.Create(left._p0, left._p1, left._p2, left._p3);
 				
-				Vector256<ulong> vUpper2 = Vector256.Create(left._p4, left._p5, left._p6, left._p7);
-				Vector256<ulong> vLower2 = Vector256.Create(left._p0, left._p1, left._p2, left._p3);
+				Vector256<ulong> vUpper2 = Vector256.Create(right._p4, right._p5, right._p6, right._p7);
+				Vector256<ulong> vLower2 = Vector256.Create(right._p0, right._p1, right._p2, right._p3);
 
 				return vUpper1 == vUpper2 && vLower1 == vLower2;
 			}
@@ -1589,8 +1589,8 @@ namespace MissingValues
 				Vector256<ulong> vUpper1 = Vector256.Create(left._p4, left._p5, left._p6, left._p7);
 				Vector256<ulong> vLower1 = Vector256.Create(left._p0, left._p1, left._p2, left._p3);
 				
-				Vector256<ulong> vUpper2 = Vector256.Create(left._p4, left._p5, left._p6, left._p7);
-				Vector256<ulong> vLower2 = Vector256.Create(left._p0, left._p1, left._p2, left._p3);
+				Vector256<ulong> vUpper2 = Vector256.Create(right._p4, right._p5, right._p6, right._p7);
+				Vector256<ulong> vLower2 = Vector256.Create(right._p0, right._p1, right._p2, right._p3);
 
 				return vUpper1 != vUpper2 || vLower1 != vLower2;
 			}
