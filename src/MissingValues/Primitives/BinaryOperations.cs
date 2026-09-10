304d070902ed7d8e0796024dd9f68c776823d38f
crookseta <pacedeno.2002@gmail.com>
2026-09-09 20:42:35 -0500

    -Added Span overloads for BinaryOperations.ReverseEndianness.

--- src/MissingValues/Primitives/BinaryOperations.cs
+++ src/MissingValues/Primitives/BinaryOperations.cs
@@ -1,5 +1,6 @@
 using System.Buffers.Binary;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
 
 namespace MissingValues.Primitives;
@@ -175,5 +176,305 @@ public static partial class BinaryOperations
             BinaryPrimitives.ReverseEndianness(value.Part0), BinaryPrimitives.ReverseEndianness(value.Part1), BinaryPrimitives.ReverseEndianness(value.Part2), BinaryPrimitives.ReverseEndianness(value.Part3),
             BinaryPrimitives.ReverseEndianness(value.Part4), BinaryPrimitives.ReverseEndianness(value.Part5), BinaryPrimitives.ReverseEndianness(value.Part6), BinaryPrimitives.ReverseEndianness(value.Part7)
         );
+    }
+
+    /// <summary>
+    /// Copies every value from <paramref name="source"/> to <paramref name="destination"/>, reversing each primitive by performing an endianness swap as part of writing each.
+    /// </summary>
+    /// <param name="source">The source span to copy.</param>
+    /// <param name="destination">The destination to which the source elements should be copied.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The <paramref name="destination"/>'s length is smaller than that of the <paramref name="source"/>.</exception>
+    public static void ReverseEndianness(ReadOnlySpan<UInt256> source, Span<UInt256> destination)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(destination.Length, source.Length);
+        if (Unsafe.AreSame(in source[0], in destination[0]) || !source.Overlaps(destination, out int elementOffset) || elementOffset < 0)
+        {
+            int i = 0;
+
+            if (Vector256.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<UInt256, Vector256<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<UInt256, Vector256<byte>>(destination);
+
+                for (; i < vectorSource.Length; i++)
+                {
+                    vectorDestination[i] = Vector256.Shuffle(
+                        vectorSource[i],
+                        Vector256.Create(
+                            (byte)31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i < source.Length; i++)
+                {
+                    UInt256 temp = source[i];
+                    destination[i] = new UInt256(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1),
+                        BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3)
+                    );
+                }
+            }
+        }
+        else
+        {
+            int i = source.Length - 1;
+            
+            if (Vector256.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<UInt256, Vector256<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<UInt256, Vector256<byte>>(destination);
+
+                for (; i >= 0; i--)
+                {
+                    vectorDestination[i] = Vector256.Shuffle(
+                        vectorSource[i],
+                        Vector256.Create(
+                            (byte)31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i >= 0; i--)
+                {
+                    UInt256 temp = source[i];
+                    destination[i] = new UInt256(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1),
+                        BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3)
+                    );
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Copies every value from <paramref name="source"/> to <paramref name="destination"/>, reversing each primitive by performing an endianness swap as part of writing each.
+    /// </summary>
+    /// <param name="source">The source span to copy.</param>
+    /// <param name="destination">The destination to which the source elements should be copied.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The <paramref name="destination"/>'s length is smaller than that of the <paramref name="source"/>.</exception>
+    public static void ReverseEndianness(ReadOnlySpan<Int256> source, Span<Int256> destination)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(destination.Length, source.Length);
+        if (Unsafe.AreSame(in source[0], in destination[0]) || !source.Overlaps(destination, out int elementOffset) || elementOffset < 0)
+        {
+            int i = 0;
+
+            if (Vector256.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<Int256, Vector256<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<Int256, Vector256<byte>>(destination);
+
+                for (; i < vectorSource.Length; i++)
+                {
+                    vectorDestination[i] = Vector256.Shuffle(
+                        vectorSource[i],
+                        Vector256.Create(
+                            (byte)31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i < source.Length; i++)
+                {
+                    Int256 temp = source[i];
+                    destination[i] = new Int256(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1),
+                        BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3)
+                    );
+                }
+            }
+        }
+        else
+        {
+            int i = source.Length - 1;
+            
+            if (Vector256.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<Int256, Vector256<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<Int256, Vector256<byte>>(destination);
+
+                for (; i >= 0; i--)
+                {
+                    vectorDestination[i] = Vector256.Shuffle(
+                        vectorSource[i],
+                        Vector256.Create(
+                            (byte)31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i >= 0; i--)
+                {
+                    Int256 temp = source[i];
+                    destination[i] = new Int256(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1),
+                        BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3)
+                    );
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Copies every value from <paramref name="source"/> to <paramref name="destination"/>, reversing each primitive by performing an endianness swap as part of writing each.
+    /// </summary>
+    /// <param name="source">The source span to copy.</param>
+    /// <param name="destination">The destination to which the source elements should be copied.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The <paramref name="destination"/>'s length is smaller than that of the <paramref name="source"/>.</exception>
+    public static void ReverseEndianness(ReadOnlySpan<UInt512> source, Span<UInt512> destination)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(destination.Length, source.Length);
+        if (Unsafe.AreSame(in source[0], in destination[0]) || !source.Overlaps(destination, out int elementOffset) || elementOffset < 0)
+        {
+            int i = 0;
+
+            if (Vector512.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<UInt512, Vector512<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<UInt512, Vector512<byte>>(destination);
+
+                for (; i < vectorSource.Length; i++)
+                {
+                    vectorDestination[i] = Vector512.Shuffle(
+                        vectorSource[i],
+                        Vector512.Create(
+                            (byte)63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 
+                            47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32,
+                            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i < source.Length; i++)
+                {
+                    UInt512 temp = source[i];
+                    destination[i] = new UInt512(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1), BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3),
+                        BinaryPrimitives.ReverseEndianness(temp.Part4), BinaryPrimitives.ReverseEndianness(temp.Part5), BinaryPrimitives.ReverseEndianness(temp.Part6), BinaryPrimitives.ReverseEndianness(temp.Part7)
+                    );
+                }
+            }
+        }
+        else
+        {
+            int i = source.Length - 1;
+            
+            if (Vector512.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<UInt512, Vector512<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<UInt512, Vector512<byte>>(destination);
+
+                for (; i >= 0; i--)
+                {
+                    vectorDestination[i] = Vector512.Shuffle(
+                        vectorSource[i],
+                        Vector512.Create(
+                            (byte)63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 
+                            47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32,
+                            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i >= 0; i--)
+                {
+                    UInt512 temp = source[i];
+                    destination[i] = new UInt512(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1), BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3),
+                        BinaryPrimitives.ReverseEndianness(temp.Part4), BinaryPrimitives.ReverseEndianness(temp.Part5), BinaryPrimitives.ReverseEndianness(temp.Part6), BinaryPrimitives.ReverseEndianness(temp.Part7)
+                    );
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Copies every value from <paramref name="source"/> to <paramref name="destination"/>, reversing each primitive by performing an endianness swap as part of writing each.
+    /// </summary>
+    /// <param name="source">The source span to copy.</param>
+    /// <param name="destination">The destination to which the source elements should be copied.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The <paramref name="destination"/>'s length is smaller than that of the <paramref name="source"/>.</exception>
+    public static void ReverseEndianness(ReadOnlySpan<Int512> source, Span<Int512> destination)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(destination.Length, source.Length);
+        if (Unsafe.AreSame(in source[0], in destination[0]) || !source.Overlaps(destination, out int elementOffset) || elementOffset < 0)
+        {
+            int i = 0;
+
+            if (Vector512.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<Int512, Vector512<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<Int512, Vector512<byte>>(destination);
+
+                for (; i < vectorSource.Length; i++)
+                {
+                    vectorDestination[i] = Vector512.Shuffle(
+                        vectorSource[i],
+                        Vector512.Create(
+                            (byte)63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 
+                            47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32,
+                            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i < source.Length; i++)
+                {
+                    Int512 temp = source[i];
+                    destination[i] = new Int512(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1), BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3),
+                        BinaryPrimitives.ReverseEndianness(temp.Part4), BinaryPrimitives.ReverseEndianness(temp.Part5), BinaryPrimitives.ReverseEndianness(temp.Part6), BinaryPrimitives.ReverseEndianness(temp.Part7)
+                    );
+                }
+            }
+        }
+        else
+        {
+            int i = source.Length - 1;
+            
+            if (Vector512.IsHardwareAccelerated)
+            {
+                var vectorSource = MemoryMarshal.Cast<Int512, Vector512<byte>>(source);
+                var vectorDestination = MemoryMarshal.Cast<Int512, Vector512<byte>>(destination);
+
+                for (; i >= 0; i--)
+                {
+                    vectorDestination[i] = Vector512.Shuffle(
+                        vectorSource[i],
+                        Vector512.Create(
+                            (byte)63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 
+                            47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32,
+                            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 
+                            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+                    );
+                }
+            }
+            else
+            {
+                for (; i >= 0; i--)
+                {
+                    Int512 temp = source[i];
+                    destination[i] = new Int512(
+                        BinaryPrimitives.ReverseEndianness(temp.Part0), BinaryPrimitives.ReverseEndianness(temp.Part1), BinaryPrimitives.ReverseEndianness(temp.Part2), BinaryPrimitives.ReverseEndianness(temp.Part3),
+                        BinaryPrimitives.ReverseEndianness(temp.Part4), BinaryPrimitives.ReverseEndianness(temp.Part5), BinaryPrimitives.ReverseEndianness(temp.Part6), BinaryPrimitives.ReverseEndianness(temp.Part7)
+                    );
+                }
+            }
+        }
     }
 }
